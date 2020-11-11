bb1a6d632386b854e0859802164c248040c4bcf2
hassan abu kwaik <hassanabukwaik80@gmail.com>
2020-11-11 20:07:12 +0200

    cr7.index

--- min.js
+++ min.js
@@ -1,12 +1,16 @@
-$(document).on('click', '.panel-heading span.clickable', function(e){
-    var $this = $(this);
-	if(!$this.hasClass('panel-collapsed')) {
-		$this.parents('.panel').find('.panel-body').slideUp();
-		$this.addClass('panel-collapsed');
-		$this.find('i').removeClass('glyphicon-chevron-up').addClass('glyphicon-chevron-down');
-	} else {
-		$this.parents('.panel').find('.panel-body').slideDown();
-		$this.removeClass('panel-collapsed');
-		$this.find('i').removeClass('glyphicon-chevron-down').addClass('glyphicon-chevron-up');
+
+$(function(){
+  $("#menu-toggle").click(function(e) {
+	  e.preventDefault();
+	  $("#wrapper").toggleClass("toggled");
+  });
+
+  $(window).resize(function(e) {
+	if($(window).width()<=768){
+	  $("#wrapper").removeClass("toggled");
+	}else{
+	  $("#wrapper").addClass("toggled");
 	}
-})
+  });
+});
+ 
