d96be3b53212eedd2de185044d70af984c56027f
hassan abu kwaik <hassanabukwaik80@gmail.com>
2020-11-12 17:59:54 +0200

    solve issues

--- bootstrap.js
+++ bootstrap.js
@@ -12,3 +12,25 @@ document.querySelector(".sidbar-background").addEventListener("click", () => {
 	document.querySelector(".sidbar").style.display = "none";
 	document.querySelector(".sidbar-background").style.display = "none"
 })
+$(document).ready(function(){
+    $('.product-photo').slick({
+        slidesToShow: 6,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 1500,
+        arrows: false,
+        dots: false,
+        pauseOnHover: false,
+        responsive: [{
+            breakpoint: 768,
+            settings: {
+                slidesToShow: 4
+            }
+        }, {
+            breakpoint: 520,
+            settings: {
+                slidesToShow: 3
+            }
+        }]
+    });
+});
